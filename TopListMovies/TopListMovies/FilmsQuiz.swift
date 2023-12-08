49f11b6d531944044b3532fb2184ecd40c9ba142
Skorodumov.D.S <eparis@atlassian.com>
2023-12-08 13:57:30 +0300

    SwiftUI_4

--- TopListMovies/TopListMovies/FilmsQuiz.swift
+++ TopListMovies/TopListMovies/FilmsQuiz.swift
@@ -8,26 +8,34 @@
 import SwiftUI
 
 struct FilmsQuiz: View {
-    
-    var randomElement: Post = PostArray().data.randomElement()!
+    @State var text : String = "What's the film title of image presented?"
+    @State var randomElement: Post = PostArray().data.randomElement()!
     var filmsArray  = PostArray().data
     var body: some View {
         VStack{
-            Text("What's the film title of image presented?")
+            Text(text)
+                .animation(.easeInOut, value: text)
             randomElement.image
-            
+                .resizable()
+                .animation(.easeInOut, value: randomElement.image)
             ForEach(filmsArray) { array in
             Button(array.title) {
                     if array.title == randomElement.title {
-                        
+                        text = "Perfectly"
+                        DispatchQueue.main.asyncAfter(deadline: .now() + 3.0)
+                        {
+                            let previousElement = randomElement
+                            let newElementArray = filmsArray.filter{$0.id != previousElement.id}
+                            randomElement = newElementArray.randomElement()!
+                            text = "What's the film title of image presented?"
+                        }
                     }else {
-                        
+                        text = "Wrong anwser, try again. What's the film title of image presented?"
                     }
                 }
                 Spacer()
             }
         }
-        .animation(.easeInOut)
     }
 }
 
